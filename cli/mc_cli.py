7ee74a82515eccf7c5d1cc49dc4d5c0cab2aa29e
Dustin <dustinsurwill@gmail.com>
2021-01-20 21:49:25 -0800

    + started making menus % more frameworking

--- cli/mc_cli.py
+++ cli/mc_cli.py
@@ -6,7 +6,7 @@ from os.path import exists
 
 class Recipe:
     inputs: list[tuple[str, int]]
-    outputs: list[tuple[str, int]]  # < 1 is percent
+    outputs: list[tuple[str, int]]  # < 1 is percent chance
 
 
 @dataclass
@@ -14,9 +14,16 @@ class Item:
     name: str
     amount: int = 0
 
+    def __add__(self, other: int):
+        self.amount += other
+
+    def __sub__(self, other: int):
+        self.amount -= other
+
 
 class Loader:
     __filename: str
+    __autosave: bool = True
 
     @classmethod
     def load(cls, filename):
@@ -31,6 +38,28 @@ class Loader:
     def save(self, filename: str = ''):
         with open(filename or self.__filename, 'wb') as file:
             pickle.dump(self, file)
+
+    def __del__(self):
+        if self.__autosave and self.__filename:
+            self.save()
+
+
+class Commands:
+    @classmethod
+    def int_select(cls, commands: list[str]) -> str:
+        print('Please select by inputting the number:')
+        for i, command in enumerate(commands):
+            print(f'{i + 1}) {command}')
+        len_commands = len(commands)
+        while True:
+            user = input('? ')
+            try:
+                user = int(user)
+                if user < 1 or user > len_commands:
+                    raise ValueError
+                return commands[user]
+            except ValueError:
+                print('Not a valid number')
 
 
 class Inventory(Loader):
@@ -57,8 +86,21 @@ class Crafter:
     def __init__(self, recipes: str = 'recipes.dat', inventory: str = 'inventory.dat'):
         self.inventory = Inventory.load(inventory)
         self.recipes = RecipeManager.load(recipes)
+        self.main_menu()
 
     def main_menu(self):
+        while True:
+            select = Commands.int_select(['Manage Inventory', 'Manage Recipes', 'Craft', 'Exit'])
+            if select == 'Manage Inventory':
+                self.inventory.manage()
+            elif select == 'Manage Recipes':
+                self.recipes.manage()
+            elif select == 'Craft':
+                self.craft()
+            elif select == 'Exit':
+                break
+
+    def craft(self):
         pass
 
 
